d3eae236c5b57ee5d112363c383b991a99702587
lasados <lasados96@gmail.com>
2020-08-24 16:10:22 +0300

    add classes

--- Supervised/RidgeRegression/ridge.py
+++ Supervised/RidgeRegression/ridge.py
@@ -1,88 +1,98 @@
-import pandas as pd
 import numpy as np
 import seaborn as sns
 import random
 
-from sklearn.datasets import load_boston
 from matplotlib import pyplot as plt
 
-boston_dataset = load_boston()
-sns.set()
 
-
-def train_dev_test_split(X, y, shuffle=True, train_size=0.7, dev_size=0.2):
+class Ridge:
     """
-    Splits given arrays of X and y on three parts.
-    Arguments:
-        X - np.array of features, X.shape = (N, nx)
-        y - np.array of target, y.shape = (N, 1)
-        N - number of samples
-    Returns:
-        [X_train, y_train], [X_dev, y_dev], [X_test, y_test]
+    Ridge regression.
+    Parameters:
+        alpha :
+        solver :
+        w_len :
+    Methods:
+        fit
+        predict
     """
+    def __init__(self, alpha=1.0, solver='gd', w_len=100, init_w=None):
+        self._w = init_w
+        self._learning_loss = None
+        self.__params = {'alpha': alpha,
+                         'solver': solver,
+                         'w_len': w_len}
 
-    N, nx = X.shape
+    def __loss(self, X, w, y_true):
+        n_samples = X.shape[0]
+        y_pred = np.dot(X, w)
+        alpha = self.__params['alpha']
 
-    assert y.shape[0] == N, "X and y arrays have different number of samples"
-    assert train_size + dev_size <= 1.0, "Bad size of sets"
+        loss = 1 / (2 * n_samples) * np.sum((y_pred - y_true) ** 2) + alpha / 2 * np.sum(w ** 2)
+        dw = 1 / n_samples * np.dot((y_pred - y_true), X) + alpha * w
 
-    idx = np.array(range(N))
-    if shuffle:
-        random.shuffle(idx)
+        return {'loss': loss, 'dw': dw}
 
-    idx_train = idx[0: int(train_size * N)]
-    idx_dev = idx[int(train_size * N): int((train_size + dev_size) * N)]
-    idx_test = idx[int((train_size + dev_size) * N):]
+    def __init_weights(self, nx):
+        """
+        Initialize weights with random numbers.
+        Arguments:
+            nx - int, X.shape = (N, nx)
+        Returns:
+            w - np.array
+        """
+        w_len = self.__params['w_len']
+        w = w_len * np.random.random(nx)
+        return w
 
-    X_train, y_train = X[idx_train], y[idx_train]
-    X_dev, y_dev = X[idx_dev], y[idx_dev]
-    X_test, y_test = X[idx_test], y[idx_test]
+    def fit(self, X_train, y_train, learning_rate=0.01, n_iter=1000, save_loss=True):
+        assert self.__params['solver'] == 'gd', "Solver does not exists yet."
 
-    return [X_train, y_train], [X_dev, y_dev], [X_test, y_test]
+        # Init weights
+        nx = X_train.shape[1]
+        w = self.__init_weights(nx)
 
+        # Init losses for each iteration
+        if save_loss:
+            loss_on_iter = {'losses': [],
+                            'iterations': list(range(n_iter))}
+        else:
+            loss_on_iter = None
 
-def standart_scaler(X_train, X_dev, X_test):
-    """
-    Scales arrays based on train set with Standart_Scale.
-    Arguments:
-        X_train, X_dev, X_test - np.arrays
-    Returns:
-        X_train_scale, X_dev_scale, X_test_scale - np.arrays
-    """
+        # Learning
+        for i in range(n_iter):
+            cache = self.__loss(X_train, w, y_train)
+            loss_current, dw_current = cache['loss'], cache['dw']
+            w = w - learning_rate * dw_current
 
-    mu = np.mean(X_train, axis=0)
-    sigma = np.std(X_train, axis=0)
+            if save_loss:
+                loss_on_iter['losses'].append(loss_current)
 
-    X_train_scale = (X_train - mu) / sigma
-    X_dev_scale = (X_dev - mu) / sigma
-    X_test_scale = (X_test - mu) / sigma
+        self._w = w
+        self._learning_loss = loss_on_iter
+        return self
 
-    return X_train_scale, X_dev_scale, X_test_scale
+    def predict(self, X):
+        w = self._w
+        assert w is not None, "Not fitted yet."
+        y_pred = np.dot(X, w)
+        return y_pred
 
+    def plot_learning_curve(self, *args, **kwargs):
+        iters = self._learning_loss['iterations']
+        losses = self._learning_loss['losses']
 
-def init_weights(nx):
-    """
-    Initialize weight with random numbers.
-    Arguments:
-        nx - int, X.shape = (N, nx)
-    Returns:
-        w - np.array
-    """
-
-    w = 100 * np.random.random(nx)
-    return w
-
-
-def ridge_loss(X_train, y_train, w, lamda=1.0):
-    N = X_train.shape[0]
-    A = np.dot(X_train, w)
-
-    Q = 1 / (2 * N) * np.sum((A - y_train) ** 2) + lamda / 2 * np.sum(w ** 2)
-
-    dw = 1 / N * np.dot((A - y_train), X_train) + lamda * w
-
-    return Q, dw
+        plt.figure(figsize=(12, 6))
+        plt.plot(iters, losses, *args, **kwargs)
+        plt.xlabel('Iteration')
+        plt.ylabel('Loss')
+        plt.title('Learning Curve')
+        plt.show()
 
 
-def update_weights(w, dw, learning_rate=0.01):
-    return w - learning_rate*dw
+
+
+
+
+
+
